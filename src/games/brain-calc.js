bcce1c5d86c8571976354b4ffad2130cc5c91ec5
Olga-5 <olgasharapo2@gmail.com>
2018-07-11 22:11:10 +0300

    correcting variable names --brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -4,21 +4,24 @@ import getRandom from '../utils/random';
 export default () => {
   const a = getRandom(0, 100);
   const b = getRandom(0, 20);
-  const numMathExpreission = getRandom(1, 3);
+  const numMathExpression = getRandom(1, 3);
   const getRandomExpression = (numRandomExpression) => {
     switch (numRandomExpression) {
       case 1:
         return cons(`${a} + ${b}`, a + b);
       case 2:
         return cons(`${a} - ${b}`, a - b);
-      default:
+      case 3:
         return cons(`${a} * ${b}`, a * b);
+      default:
+        return 'expression not found';
     }
   };
-  const getExpression = randomExpression => car(randomExpression);
-  const getCalcExpression = randomExpression => cdr(randomExpression);
+  const randomExpression = getRandomExpression(numMathExpression);
+  const getExpression = expressionForShowAndCalced => car(expressionForShowAndCalced);
+  const getCalcExpression = expressionForShowAndCalced => cdr(expressionForShowAndCalced);
 
-  const expression = getExpression(getRandomExpression(numMathExpreission));
-  const trueAnswer = String(getCalcExpression(getRandomExpression(numMathExpreission)));
+  const expression = getExpression(randomExpression);
+  const trueAnswer = String(getCalcExpression(randomExpression));
   return cons(expression, trueAnswer);
 };
