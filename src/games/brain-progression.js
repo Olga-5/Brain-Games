a1b8f778a4c121de6bdb2625162b78a4cd5dcd5d
Olga-5 <olgasharapo2@gmail.com>
2018-07-12 14:19:15 +0300

    fix brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,33 +1,31 @@
 import { cons, car, cdr } from 'hexlet-pairs';
 import getRandom from '../utils/random';
 
-const generateProgression = (startItem) => {
-  let nextStep = startItem;
-  let generatedProgression = '';
-  let missingItem = 0;
-  const positionInProgression = getRandom(1, 9);
-  const startPosition = 1;
-  const finalPosition = 10;
-  const stepInProgression = 2;
-  for (let positionCount = startPosition; positionCount < finalPosition; positionCount += 1) {
-    if (positionCount === positionInProgression) {
-      missingItem = nextStep;
-      generatedProgression += '.. ';
-      nextStep += stepInProgression;
+const generateProgression = (startItem, positionMissingItem, stepInProgression) => {
+  const iter = (positionsCount, resultOfGenetation, missingItem, nextItem) => {
+    const positionLastItem = 10;
+    if (positionsCount > positionLastItem) {
+      return cons(resultOfGenetation, missingItem);
+    } if (positionsCount === positionMissingItem) {
+      return iter(positionsCount + 1, `${resultOfGenetation}.. `, nextItem, nextItem + stepInProgression);
     }
-    generatedProgression += `${String(nextStep)} `;
-    nextStep += stepInProgression;
-  }
-  return cons(generatedProgression, missingItem);
+    return iter(positionsCount + 1, `${resultOfGenetation}${String(nextItem)} `, missingItem, nextItem + stepInProgression);
+  };
+  const positionFirstItem = 1;
+  const initialResult = '';
+  const initialValueMissingItem = 0;
+  return iter(positionFirstItem, initialResult, initialValueMissingItem, startItem);
 };
 
 export default () => {
   const startItem = getRandom(0, 10);
-  const progression = generateProgression(startItem);
-  const getGeneratedProgression = progressionAndItem => car(progressionAndItem);
-  const getMissingItem = progressionAndItem => cdr(progressionAndItem);
+  const positionMissingItem = getRandom(1, 9);
+  const stepInProgression = 2;
+  const resultOfGenetation = generateProgression(startItem, positionMissingItem, stepInProgression);
+  const getGeneratedProgression = progressionAndMissingItem => car(progressionAndMissingItem);
+  const getMissingItem = progressionAndMissingItem => cdr(progressionAndMissingItem);
 
-  const expression = getGeneratedProgression(progression);
-  const trueAnswer = String(getMissingItem(progression));
+  const expression = getGeneratedProgression(resultOfGenetation);
+  const trueAnswer = String(getMissingItem(resultOfGenetation));
   return cons(expression, trueAnswer);
 };
