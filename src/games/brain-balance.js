0cd7e4b789733fcf72895eb6096086c99071369f
Olga-5 <olgasharapo2@gmail.com>
2018-07-15 16:30:04 +0300

    correcting variable names --brain-balance

--- src/games/brain-balance.js
+++ src/games/brain-balance.js
@@ -4,15 +4,15 @@ import getRandom from '../utils/random';
 const compareNumbers = (a, b) => a - b;
 
 const balance = (numberAsString) => {
-  const sortArray = numberAsString.split('').sort(compareNumbers).map(Number);
-  const lengthArray = sortArray.length;
-  const firstItemOfArray = sortArray[0];
-  const lastItemOfArray = sortArray[lengthArray - 1];
+  const sortedArray = numberAsString.split('').sort(compareNumbers).map(Number);
+  const lengthArray = sortedArray.length;
+  const firstItemOfArray = sortedArray[0];
+  const lastItemOfArray = sortedArray[lengthArray - 1];
   const permissibleValue = 1;
   if (lastItemOfArray - firstItemOfArray <= permissibleValue) {
-    return sortArray.join('');
+    return sortedArray.join('');
   }
-  const changeArrayItems = (currentValue, indexCurrentItem, array) => {
+  const balanceArrayItems = (currentValue, indexCurrentItem, array) => {
     const indexFirstItemOfArray = 0;
     if (indexCurrentItem === indexFirstItemOfArray) {
       return currentValue + 1;
@@ -23,7 +23,7 @@ const balance = (numberAsString) => {
     }
     return currentValue;
   };
-  const changedArray = sortArray.map(changeArrayItems);
+  const changedArray = sortedArray.map(balanceArrayItems);
   return balance(changedArray.join(''));
 };
 
